d1ebc4911fc94b9eb0bb02951aa999a6983477e1
arranzeyuwang <zywangx@gmail.com>
2023-03-09 23:28:42 -0500

    feat: add formal page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/style-prop-object */
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -5,12 +6,51 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <head>
+        <script src="https://cdn.jsdelivr.net/npm/vega@5.22.1"></script>
+        <script src="https://cdn.jsdelivr.net/npm/vega-lite@5.6.1"></script>
+        <script src="https://cdn.jsdelivr.net/npm/vega-embed@6.21.2"></script>
+      </head>
       <header className="App-header">
-        <h2>UNC Lead Explosure</h2>
-        <p>
-          This project tend to visualize and explain recent concerns about lead explosure in UNC.
-        </p>
+        <h2>UNC Lead Exposure</h2>
       </header>
+      <p className="App-body">
+        <p>
+          &emsp;The safety of water systems severely affects our life in UNC.
+          In last November, many of us received emails form the department 
+          about the lead tested in water fixtures of the Brooks building.
+        </p>
+        <p>
+          &emsp;However, if we look at more details published on the university website,
+          we'll see there're tens of buildings was reported to lead exposure.
+          Despite these datasets being published, the table format with complex text 
+          still makes it hard to get an at-a-glance understanding.
+        </p>
+        <p>
+          &emsp;This website tends to visualize and explain concerns about
+          where, when, and how lead exposure happened in UNC,
+          to make the published dataset easier to understand.
+        </p>
+        <p>
+          &emsp;Where -- buildings that were reported lead exposure:
+        </p>
+        <div id="where"></div>
+        <p>
+          &emsp;When -- number of lead exposure buildings reported by date:
+        </p>
+        <div id="when"></div>
+        <p>
+          &emsp;How -- (how much and how frequent):
+          tested lead value and number/percentage of fixtures of lead across buildings:
+        </p>
+        <div id="how"></div>
+        <p>
+          &emsp;Dataset source at:&nbsp;
+          <a href="https://ehs.unc.edu/topics/campus-drinking-water/drinking-water-testing-results/">
+            https://ehs.unc.edu/topics/campus-drinking-water/drinking-water-testing-results/
+          </a>
+        </p> 
+      </p>
     </div>
   );
 }
